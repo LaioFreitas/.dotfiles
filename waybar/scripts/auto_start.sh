73e6eaf325f416ba1bd7ba1d40d2c61fca763d5a
LaioFreitas <laiofrsitas447@gmail.com>
2026-03-09 00:15:00 -0300

    -

---
+++ waybar/scripts/auto_start.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+cd ~/.local/share/omarchy/themes/
+
+ln -s ~/.config/waybar/color_themes/color_theme_catppuccin.css catppuccin/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_catppuccin_latte.css catppuccin-latte/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_ethereal.css ethereal/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_everforest.css everforest/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_flexoki_light.css flexoki-light/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_gruvbox.css gruvbox/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_hackerman.css hackerman/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_matteBlack.css matte-black/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_miasma.css miasma/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_nord.css nord/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_osaka_jade.css osaka-jade/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_ristretto.css ristretto/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_rosePine.css rose-pine/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_tokyoNight.css tokyo-night/colorTheme
+ln -s ~/.config/waybar/color_themes/color_theme_vantaBlack.css vantablack/colorTheme
+
+
+
