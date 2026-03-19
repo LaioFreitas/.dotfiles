a4e51086a3b3cf1ef43bd11a545dbfad92f1e1f9
LaioFreitas <laiofrsitas447@gmail.com>
2026-03-19 20:39:33 -0300

    -

--- waybar/scripts/auto_start.sh
+++ waybar/scripts/auto_start.sh
@@ -2,21 +2,20 @@
 
 cd ~/.local/share/omarchy/themes/
 
-ln -s ~/.config/waybar/color_themes/color_theme_catppuccin.css catppuccin/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_catppuccin_latte.css catppuccin-latte/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_ethereal.css ethereal/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_everforest.css everforest/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_flexoki_light.css flexoki-light/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_gruvbox.css gruvbox/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_hackerman.css hackerman/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_matteBlack.css matte-black/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_miasma.css miasma/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_nord.css nord/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_osaka_jade.css osaka-jade/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_ristretto.css ristretto/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_rosePine.css rose-pine/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_tokyoNight.css tokyo-night/colorTheme
-ln -s ~/.config/waybar/color_themes/color_theme_vantaBlack.css vantablack/colorTheme
-
-
+ln -s ~/.config/waybar/color_themes/color_theme_catppuccin.css catppuccin/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_catppuccin_latte.css catppuccin-latte/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_ethereal.css ethereal/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_everforest.css everforest/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_flexoki_light.css flexoki-light/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_gruvbox.css gruvbox/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_hackerman.css hackerman/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_matteBlack.css matte-black/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_miasma.css miasma/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_nord.css nord/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_osaka_jade.css osaka-jade/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_ristretto.css ristretto/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_rosePine.css rose-pine/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_tokyoNight.css tokyo-night/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_vantaBlack.css vantablack/colorTheme.css
+ln -s ~/.config/waybar/color_themes/color_theme_white.css white/colorTheme.css
 
